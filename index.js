fe4003a0768d3a690bcaf65bd96f82e8614a1054
Karmstead01 <136016271+Karmstead01@users.noreply.github.com>
2023-06-21 19:42:01 -0500

    homework
    about

---
+++ index.js
@@ -0,0 +1,40 @@
+//VARIABLES
+//let, var, const
+
+// const name = "kierstin";
+
+// name = "jamie";
+
+// console.log(name); //Print
+
+// console.log("Hello world");
+
+const name = "pizza";
+const topping = "pineapple";
+/* adding two strings;
+concatenation */
+
+console.log(name, topping);
+
+// TEMPLATE LITERALS// turns everything to a string
+console.log(`${name} ${topping}`);
+
+console.log("32" + "100" + "string");
+
+// integer
+
+// booleans
+
+// false, undefined, null, 0
+
+// let num = name;
+
+// console.log(Boolean(num));
+
+let firstName = prompt("what is your name?");
+let age2 = prompt("What is your age?");
+let hobbie3 = prompt("what is your hobbie?");
+
+alert(
+  `Hi my name is ${firstName} and my age is ${age2} and my favorite hobbie is ${hobbie3}`
+);
